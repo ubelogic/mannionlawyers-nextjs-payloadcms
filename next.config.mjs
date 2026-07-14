0f4668fe5cc03cc859944d2ca7338cce87220e54
fflope <127733852+fflope@users.noreply.github.com>
2026-07-14 20:33:27 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
